fec76cd816b4a38bd8fc006de1b08883629ecb34
Takhir Galimzianov <tariel@post.com>
2023-10-15 23:07:52 +0400

    Add project files.

---
+++ Array2DSort/Array2DSort/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Array2DSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Исходный двумерный массив
+            int[,] arr2d = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 0 } };
+
+            // Получаем размеры массива
+            int rows = arr2d.GetLength(0);
+            int cols = arr2d.GetLength(1);
+
+            // Преобразуем двумерный массив в одномерный для сортировки
+            int[] arr1d = new int[rows * cols];
+            int index = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    arr1d[index] = arr2d[i, j];
+                    index++;
+                }
+            }
+
+            // Сортируем одномерный массив
+            Array.Sort(arr1d);
+
+            // Восстанавливаем отсортированные значения в новый двумерный массив
+            int[,] arrSorted = new int[rows, cols];
+            index = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    arrSorted[i, j] = arr1d[index];
+                    index++;
+                }
+            }
+
+            // Выводим отсортированный двумерный массив в консоль
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(arrSorted[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Calc/calc/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace calc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+
+                List<string> argumentsList = args.ToList();
+
+                //Кол-во всех операций в выражении
+                int operCount = argumentsList.Where(x => x.Contains('*') || x.Contains('/') || x.Contains('+') || x.Contains('-')).Count();
+                double result;
+
+                for (int i = 0; i < operCount; i++)
+                {
+                    //Определение первой самой операции (/ или *)
+                    string? oper = argumentsList.FirstOrDefault(x => x.Contains('*') || x.Contains('/'));
+                    //Определение номера элемента операции (/ или *)
+                    int index = argumentsList.Select((arg, i) => new { Argument = arg, Index = i })
+                            .FirstOrDefault(x => x.Argument.Contains('*') || x.Argument.Contains('/'))?.Index ?? -1;
+
+                    if (oper == null)
+                    {
+                        //Определение первой самой операции (+ или -)
+                        oper = argumentsList.FirstOrDefault(x => x.Contains('+') || x.Contains('-'));
+                        //Определение номера элемента операции (+ или -)
+                        index = argumentsList.Select((arg, i) => new { Argument = arg, Index = i })
+                                .FirstOrDefault(x => x.Argument.Contains('+') || x.Argument.Contains('-'))?.Index ?? -1;
+
+                    }
+
+                    switch (oper)
+                    {
+                        case "/":
+                            //Результат операции
+                            result = Convert.ToDouble(argumentsList[index - 1]) / Convert.ToDouble(argumentsList[index + 1]);
+                            //Результат помещается вместо исполненной операции
+                            argumentsList[index] = result.ToString();
+                            //Удаляется элемент первого операнда
+                            argumentsList.RemoveAt(index - 1);
+                            //Удаляется элемент второго операнда
+                            argumentsList.RemoveAt(index);
+
+                            break;
+
+                        case "*":
+                            result = Convert.ToDouble(argumentsList[index - 1]) * Convert.ToDouble(argumentsList[index + 1]);
+                            argumentsList[index] = result.ToString();
+                            argumentsList.RemoveAt(index - 1);
+                            argumentsList.RemoveAt(index);
+
+                            break;
+
+                        case "+":
+                            result = Convert.ToDouble(argumentsList[index - 1]) + Convert.ToDouble(argumentsList[index + 1]);
+                            argumentsList[index] = result.ToString();
+                            argumentsList.RemoveAt(index - 1);
+                            argumentsList.RemoveAt(index);
+
+                            break;
+
+                        case "-":
+                            result = Convert.ToDouble(argumentsList[index - 1]) - Convert.ToDouble(argumentsList[index + 1]);
+                            argumentsList[index] = result.ToString();
+                            argumentsList.RemoveAt(index - 1);
+                            argumentsList.RemoveAt(index);
+
+                            break;
+
+                    }
+
+                }
+                //Последний оставщийся результат выполнения
+                result = Convert.ToDouble(argumentsList[0]);
+                Console.WriteLine(String.Join(" ", args) + " = " + result.ToString());
+            }
+            else
+            {
+                Console.WriteLine("args isn't available!");
+            }
+
+        }
+
+    }
+}
